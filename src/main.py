fe9e5013f679772e5c9fff9de12f21458d211f6f
a101a78 <heavyobject@naver.com>
2024-06-04 01:00:23 +0900

    ♻ refactor: Remove timestamps

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ class VideoProcessor(threading.Thread):
             boxes = results[0].boxes
             for box in boxes:
                 x1, y1, x2, y2 = box.xyxy.cpu().numpy()[0]
-                self.queue.put((self.camera_id, x1, y1, x2, y2, time.time()))
+                self.queue.put((self.camera_id, x1, y1, x2, y2))
         self.logger.log(f'Stopping video processing for camera {self.camera_id}')
         self.cap.release()
 
@@ -50,9 +50,8 @@ class CommonCoordinateSystem:
         self.lock = threading.Lock()
         self.logger = logger
 
-    def update(self, camera_id, x1, y1, x2, y2, timestamp):
+    def update(self, camera_id, x1, y1, x2, y2):
         with self.lock:
-            self.logger.log(f'Updating coordinates for camera {camera_id} at timestamp {timestamp}')
             center_x = (x1 + x2) / 2
             center_y = (y1 + y2) / 2
             common_x = center_x / config.COMMON_COORDINATE_SYSTEM_SCALE
@@ -62,7 +61,7 @@ class CommonCoordinateSystem:
             for obj_id, data in self.objects.items():
                 dist = np.sqrt((data['cx'] - common_x) ** 2 + (data['cy'] - common_y) ** 2)
                 if dist < config.COORDINATE_MATCH_THRESHOLD:
-                    self.objects[obj_id]['boxes'].append((camera_id, x1, y1, x2, y2, timestamp))
+                    self.objects[obj_id]['boxes'].append((camera_id, x1, y1, x2, y2))
                     self.objects[obj_id]['cx'] = common_x
                     self.objects[obj_id]['cy'] = common_y
                     matched = True
@@ -70,7 +69,7 @@ class CommonCoordinateSystem:
 
             if not matched:
                 self.objects[self.next_id] = {
-                    'boxes': [(camera_id, x1, y1, x2, y2, timestamp)],
+                    'boxes': [(camera_id, x1, y1, x2, y2)],
                     'cx': common_x,
                     'cy': common_y
                 }
@@ -95,7 +94,7 @@ class GUI:
         self.screen.fill(config.GUI_SETTINGS["BACKGROUND_COLOR"])
         objects = self.common_coord_system.get_objects()
         for obj_id, data in objects.items():
-            for (camera_id, x1, y1, x2, y2, timestamp) in data['boxes']:
+            for (camera_id, x1, y1, x2, y2) in data['boxes']:
                 center_x = (x1 + x2) / 2
                 center_y = (y1 + y2) / 2
                 pygame.draw.circle(self.screen, config.GUI_SETTINGS["CIRCLE_COLOR"], (int(center_x), int(center_y)),
@@ -136,8 +135,8 @@ def main():
             while not stop_event.is_set():
                 for q in queues:
                     while not q.empty():
-                        camera_id, x1, y1, x2, y2, timestamp = q.get()
-                        common_coord_system.update(camera_id, x1, y1, x2, y2, timestamp)
+                        camera_id, x1, y1, x2, y2 = q.get()
+                        common_coord_system.update(camera_id, x1, y1, x2, y2)
                 time.sleep(config.QUEUE_PROCESS_DELAY)
             logger.log('Stopping queue processing')
 
